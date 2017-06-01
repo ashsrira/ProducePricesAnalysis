27f9ef87d4a2d02073edd115bbd4e553b1494f71
Ashwath.Sriram <ashwath2009@gmail.com>
2017-06-01 01:15:40 -0700

    Update yearly_reducer.py

--- yearly_reducer.py
+++ yearly_reducer.py
@@ -4,15 +4,6 @@
 import sys
 
 def get_date_city_value(set_value):
-    #function to parse and get the date from
-    #print set_value
-    #date  = set_value.replace(')','').replace('(','').split(',')[1]
-    #date = date.replace("'",'')
-    #city  = set_value.replace(')','').replace('(','').split(',')[0]
-    #city = city.replace("'",'')
-    #city = city.replace(" ",'')
-
-    #alternate implementations
     temp = set_value.translate(None,'()')
     temp = temp.split(',')
     if len(temp) != 2:
@@ -36,10 +27,8 @@ def main(separator='\t'):
     month_value = None
     month_list = []
     value_list = []
-    #data = read_input(sys.stdin)
+    
     for line in sys.stdin:
-        #print "LINE GOT is :"
-        #print line
         line = line.split('\t')
         if len(line) != 2:
             print "NA"
@@ -47,32 +36,24 @@ def main(separator='\t'):
             current_city,date =get_date_city_value(line[0])
             if current_city == date == 'NA':
                 continue
-            #print "CURRENT CITY::>" + str(current_city)
             if current_city == prev_city:
-                value = line[1]
-                #print "VALUE IS " + str(value)
                 price = line[1]
                 date_value = get_year(date)
-                #print "DATE VALUE::> " + str(date_value)
                 if date_value == month_value:
                     value_list.append(price)
                 else:
-                    #print "DATE VALUE IS NOT THE SAME AS MONTH VALUE"
                     if month_value != None:                
                         total = sum(int(i) for i in value_list)
                         avg = total/len(value_list)
                         value_list = []
                         month_list.append((month_value,avg))
-                        #print "MONTH LIST :::> " + str(month_list)
                         month_value = date_value
                         value_list.append(price)
                     else:
                         month_value = date_value
                         value_list.append(price)
-                #print "Value LIST :::> " + str(value_list)
             else:
                 if prev_city != None:
-                    #print "Moving to next city"
                     if ((month_value != None) and (len(value_list) != 0)):
                         total = sum(int(i) for i in value_list)
                         avg = total/len(value_list)
@@ -82,18 +63,12 @@ def main(separator='\t'):
                 value_list = []
                 month_value = None
                 value = line[1]
-                #print "VALUE ::>" + str(value)
                 value = line[1]
                 current_city,date =get_date_city_value(line[0])
                 price=line[1]
-                #print "DATE::>" + date
-                #print "PRICE::>" + str(price)
                 month_value = get_year(date)
-                #print "MONTH_VALUE::>" + str(month_value)
                 value_list.append(price)
-                #print value_list
                 prev_city = current_city
-                #print "PREV CITY::>" + str(prev_city)
 
     if ((month_value != None) and (len(value_list) != 0)):
         total = sum(int(i) for i in value_list)
@@ -105,8 +80,3 @@ def main(separator='\t'):
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
